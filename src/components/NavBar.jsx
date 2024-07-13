8da03855302a871d04ed24f9745e7f6b6bec5566
Jeramie <forbesjeramie@gmail.com>
2024-07-13 17:47:19 -0400

    changed form submission from formsubmit.co to formspree.io

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import GitHub from "../assets/images/github.svg";
 // import Twitter from "../assets/images/twitter.svg";
 import { HashLink } from "react-router-hash-link";
 import { BrowserRouter as Router } from "react-router-dom";
-import Switch from "./Switch";
+// import Switch from "./Switch";
 
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
@@ -34,7 +34,72 @@ export const NavBar = () => {
   return (
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-        {japaneseToggled ? null : (
+        {japaneseToggled ? (
+          <Container>
+            <Navbar.Brand href="/">
+              <img src={logo} className="logo" alt="Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+              <span className="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
+            {/* <div className="nav-links"> */}
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link
+                  href="#home"
+                  className={
+                    activeLink === "home" ? "active navbar-link" : "navbar-link"
+                  }
+                  onClick={() => onUpdateActiveLink("home")}
+                >
+                  ホーム
+                </Nav.Link>
+                <Nav.Link
+                  href="#skills"
+                  className={
+                    activeLink === "skills"
+                      ? "active navbar-link"
+                      : "navbar-link"
+                  }
+                  onClick={() => onUpdateActiveLink("skills")}
+                >
+                  スキル
+                </Nav.Link>
+                <Nav.Link
+                  href="#projects"
+                  className={
+                    activeLink === "projects"
+                      ? "active navbar-link"
+                      : "navbar-link"
+                  }
+                  onClick={() => onUpdateActiveLink("projects")}
+                >
+                  プロジェクト
+                </Nav.Link>
+              </Nav>
+              <span className="navbar-text">
+                <div className="social-icon">
+                  <a href="https://www.linkedin.com/in/jeramieforbes/">
+                    <img src={LinkedIn} alt="LinkedIn" />
+                  </a>
+                  <a href="https://github.com/jeramief">
+                    <img src={GitHub} alt="" />
+                  </a>
+                  {/* <a href="https://twitter.com/forbes_jeramie">
+                  <img src={Twitter} alt="" />
+                </a> */}
+                </div>
+                <HashLink to="#connect">
+                  <button className="vvd">
+                    <span>接続しましょう</span>
+                  </button>
+                </HashLink>
+              </span>
+            </Navbar.Collapse>
+            <input type="checkbox" onChange={() => setJapaneseToggled(false)} />
+            {/* </div> */}
+          </Container>
+        ) : (
           <Container>
             <Navbar.Brand href="/">
               <img src={logo} className="logo" alt="Logo" />
@@ -96,10 +161,7 @@ export const NavBar = () => {
                 </HashLink>
               </span>
             </Navbar.Collapse>
-            <Switch
-              japaneseToggled={japaneseToggled}
-              onToggle={() => setJapaneseToggled(!japaneseToggled)}
-            />
+            <input type="checkbox" onChange={() => setJapaneseToggled(true)} />
             {/* </div> */}
           </Container>
         )}
