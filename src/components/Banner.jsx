2d1df6a72790ce0713b65b679a3c3c8ec5b53ec2
Jeramie <forbesjeramie@gmail.com>
2024-07-22 23:44:13 -0400

    translating

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,8 +6,13 @@ import "animate.css";
 import TrackVisibility from "react-on-screen";
 import { BrowserRouter as Router } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import { useSelector } from "react-redux";
 
 export const Banner = () => {
+  const japaneseToggled = useSelector(
+    (state) => state.languageState.japaneseToggled
+  );
+
   // const [loopNum, setLoopNum] = useState(0);
   // const [isDeleting, setIsDeleting] = useState(false);
   // const [text, setText] = useState("");
@@ -61,18 +66,19 @@ export const Banner = () => {
         <Container>
           <Row className="aligh-items-center">
             <Col xs={12} md={6} xl={7}>
-              <TrackVisibility>
-                {({ isVisible }) => (
-                  <div
-                    className={
-                      isVisible ? "animate__animated animate__fadeIn" : ""
-                    }
-                  >
-                    <span className="tagline">Welcome to my Portfolio</span>
-                    <h1>
-                      {/* {`Hi! I'm Jeramie,`}{" "} */}
-                      JERAMIE FORBES
-                      {/* <span
+              {japaneseToggled ? (
+                <TrackVisibility>
+                  {({ isVisible }) => (
+                    <div
+                      className={
+                        isVisible ? "animate__animated animate__fadeIn" : ""
+                      }
+                    >
+                      <span className="tagline">ポートフォリオにようこそ</span>
+                      <h1>
+                        {/* {`Hi! I'm Jeramie,`}{" "} */}
+                        JERAMIE FORBES
+                        {/* <span
                       className="txt-rotate"
                       // eslint-disable-next-line react/no-unknown-property
                       dataPeriod="1000"
@@ -80,34 +86,67 @@ export const Banner = () => {
                     >
                       <span className="wrap">{text}</span>
                     </span> */}
-                    </h1>
-                    <p>
-                      I transitioned from a technician role to a self-taught
-                      software engineer by embarking on a personal learning
-                      journey driven by my aspiration to make a positive impact
-                      on both people and the environment. Through dedicated
-                      self-study and hands-on projects, I acquired proficiency
-                      in software development methodologies, and the ability to
-                      create robust, scalable software solutions.I transitioned
-                      from a technician role to a self-taught software engineer
-                      by embarking on a personal learning journey driven by my
-                      aspiration to make a positive impact on both people and
-                      the environment. Through dedicated self-study and hands-on
-                      projects, I acquired proficiency in software development
-                      methodologies, and the ability to create robust, scalable
-                      software solutions.
-                    </p>
+                      </h1>
+                      <p>
+                        私は技術者の役割から独学に移行しました
+                        ソフトウェアエンジニアは、個人的な学習に着手します
+                        ポジティブにしたいという私の願望に駆り立てられた旅
+                        人と環境の両方への影響。
+                      </p>
 
-                    <HashLink to="#connect">
-                      <button>
-                        <span>
-                          HIRE ME! <ArrowRightCircle size={25} />
-                        </span>
-                      </button>
-                    </HashLink>
-                  </div>
-                )}
-              </TrackVisibility>
+                      <HashLink to="#connect">
+                        <button>
+                          <span>
+                            私を雇うてください！ <ArrowRightCircle size={25} />
+                          </span>
+                        </button>
+                      </HashLink>
+                    </div>
+                  )}
+                </TrackVisibility>
+              ) : (
+                <TrackVisibility>
+                  {({ isVisible }) => (
+                    <div
+                      className={
+                        isVisible ? "animate__animated animate__fadeIn" : ""
+                      }
+                    >
+                      <span className="tagline">Welcome to my Portfolio</span>
+                      <h1>
+                        {/* {`Hi! I'm Jeramie,`}{" "} */}
+                        JERAMIE FORBES
+                        {/* <span
+                  className="txt-rotate"
+                  // eslint-disable-next-line react/no-unknown-property
+                  dataPeriod="1000"
+                  data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
+                >
+                  <span className="wrap">{text}</span>
+                </span> */}
+                      </h1>
+                      <p>
+                        I transitioned from a technician role to a self-taught
+                        software engineer by embarking on a personal learning
+                        journey driven by my aspiration to make a positive
+                        impact on both people and the environment. Through
+                        dedicated self-study and hands-on projects, I acquired
+                        proficiency in software development methodologies, and
+                        the ability to create robust, scalable software
+                        solutions.
+                      </p>
+
+                      <HashLink to="#connect">
+                        <button>
+                          <span>
+                            HIRE ME! <ArrowRightCircle size={25} />
+                          </span>
+                        </button>
+                      </HashLink>
+                    </div>
+                  )}
+                </TrackVisibility>
+              )}
             </Col>
             <Col xs={12} md={6} xl={5}>
               <TrackVisibility>
